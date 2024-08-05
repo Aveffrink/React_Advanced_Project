040f5e24d5b585d0c1dfc24ec581127991c70a95
Astrid van Effrink <astridvaneffrink@hotmail.com>
2024-08-05 13:55:44 +0200

    Removed Event because unclear what it was used for

--- react_advanced_events/src/components/Navigation.jsx
+++ react_advanced_events/src/components/Navigation.jsx
@@ -1,4 +1,4 @@
-import { Box, Heading, List, ListItem } from "@chakra-ui/react";
+import { Box, Heading } from "@chakra-ui/react";
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -6,23 +6,9 @@ export const Navigation = () => {
   return (
     <Box background="green.600">
       <nav>
-        <List
-          display="flex"
-          flexDirection="row"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <ListItem>
-            <Heading color="white" padding="1rem">
-              <Link to="/">Events</Link>
-            </Heading>
-          </ListItem>
-          <ListItem>
-            <Heading color="white" padding="1rem">
-              <Link to="/event/1">Event</Link>
-            </Heading>
-          </ListItem>
-        </List>
+        <Heading color="white" padding="1rem" align="center">
+          <Link to="/">Events</Link>
+        </Heading>
       </nav>
     </Box>
   );
